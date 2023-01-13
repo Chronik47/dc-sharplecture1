20cb6516dfdda0f3efba74fa8a36a38e3fae264f
Chronik <vladislamakarevich@mail.ru>
2023-01-13 12:21:20 +0300

    Сложение

---
+++ Дкод003Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿// Варианты записи кода при сложении чисел.
+
+
+// int numberA = 3;
+// int numberB = 5;
+// Console.WriteLine(numberA + numberB);
+
+
+// int numberA = 39;
+// int numberB = 5;
+// int result = numberA + numberB;
+// Console.WriteLine(numberA + numberB);
