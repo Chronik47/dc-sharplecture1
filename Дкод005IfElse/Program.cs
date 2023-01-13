e9da54812e994c289232025f1c1a987bf75bc9da
Chronik <vladislamakarevich@mail.ru>
2023-01-13 18:52:24 +0300

    if и else, приветствие любимого пользователя

---
+++ Дкод005IfElse/Program.cs
@@ -0,0 +1,34 @@
+﻿//Приветствие любимого пользователя.
+
+
+// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+
+// if(username == "Маша")
+// {
+//     Console.WriteLine("Ура, это же МАША!");
+// }
+// else
+// {
+//     Console.Write("Привет,");
+//     Console.WriteLine(username);
+// }
+
+
+
+
+//ToLower -- позволяет не обращать внимание на регистр букв.
+
+
+// Console.Write("Введите имя пользователя: ");
+// string username = Console.ReadLine();
+
+// if(username.ToLower() == "маша")
+// {
+//     Console.WriteLine("Ура, это же МАША!");
+// }
+// else
+// {
+//     Console.Write("Привет,");
+//     Console.WriteLine(username);
+// }
