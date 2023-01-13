734d9d7c7f525b9c786995900fd37ee4dfef4c90
Chronik <vladislamakarevich@mail.ru>
2023-01-13 20:02:42 +0300

    нахождение наибольшего числа. база

---
+++ Дкод006/Program.cs
@@ -0,0 +1,21 @@
+﻿//Нахождение наибольшего числа. База.
+
+
+// int a = 10;
+// int b = 2;
+// int c = 8;
+// int d = 15;
+// int e = 4;
+
+// int max = a;
+
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// if (d > max) max = d;
+// if (e > max) max = e;
+
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
