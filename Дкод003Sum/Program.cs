c8fe8b268e984e917ad945002672b7167b2c556e
Chronik <vladislamakarevich@mail.ru>
2023-01-13 13:28:55 +0300

    сложение рандомных чисел

--- Дкод003Sum/Program.cs
+++ Дкод003Sum/Program.cs
@@ -9,4 +9,16 @@
 // int numberA = 39;
 // int numberB = 5;
 // int result = numberA + numberB;
-// Console.WriteLine(numberA + numberB);
+// Console.WriteLine(result);
+
+
+
+//Сложение рандомных чисел.
+
+// int numberA = new Random().Next(1,10); //(1,2,3....9)
+// Console.WriteLine(numberA);
+// int numberB = new Random().Next(1,10);
+// Console.WriteLine(numberB);
+// int result = numberA + numberB;
+// Console.WriteLine(result);
+
