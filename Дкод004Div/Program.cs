9a947c3a7e70a3767514c803e255d0de043cc3c4
Chronik <vladislamakarevich@mail.ru>
2023-01-13 12:29:49 +0300

    деление

---
+++ Дкод004Div/Program.cs
@@ -0,0 +1,13 @@
+﻿// Деление на примере с целыми и вещественными числами. Использование int и double.
+
+
+
+// int numberA = 10;
+// int numberB = 2;
+// Console.WriteLine(numberA / numberB);
+
+
+
+// double numberA = 11;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
