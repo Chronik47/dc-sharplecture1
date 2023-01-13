10804ce708dc2bd3df2f75c00594ebcd5838c5cf
Chronik <vladislamakarevich@mail.ru>
2023-01-13 12:05:41 +0300

    Hello Uzer

---
+++ Дкод002HelloUzer/Program.cs
@@ -0,0 +1,35 @@
+﻿// ПРИВЕТСВИЕ ПОЛЬЗОВАТЕЛЯ.
+
+
+
+// 1. Вывод в столбик.
+
+// Console.WriteLine ("Введите ваше имя: ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, ");
+// Console.WriteLine(username);
+
+
+// 2. Вывод в строку.
+    
+// Console.Write ("Введите ваше имя: ");
+// string username = Console.ReadLine();
+// Console.Write ("Привет, ");
+// Console.Write (username);
+
+
+// 3.Вывод с восклицательным знаком.
+
+// Console.Write ("Введите ваше имя: ");
+// string username = Console.ReadLine();
+// Console.Write ("Привет, ");
+// Console.Write (username + "!");
+
+
+// 4. Конструкция кода с консолью в одну строку.
+
+Console.Write ("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write ("Привет, " + username + "!" );
+
+
