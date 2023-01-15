b891891c7b0e92860fc07c4000a39392fc04bc50
Chronik <vladislamakarevich@mail.ru>
2023-01-15 20:08:40 +0300

    заслешила лишнюю строку

--- Дкод006/Program.cs
+++ Дкод006/Program.cs
@@ -1,21 +1,22 @@
 ﻿//Нахождение наибольшего числа. База.
 
 
-// int a = 10;
-// int b = 2;
-// int c = 8;
-// int d = 15;
-// int e = 4;
+int a = 15;
+int b = 2;
+int c = 8;
+int d = 10;
+int e = 4;
 
-// int max = a;
+int max = a;
 
 // if (a > max) max = a;
-// if (b > max) max = b;
-// if (c > max) max = c;
-// if (d > max) max = d;
-// if (e > max) max = e;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
 
-// Console.Write("max = ");
-// Console.WriteLine(max);
+Console.Write("max = ");
+Console.WriteLine(max);
 
 
+  
